9274b4606e977dda419db8c73368b32773993a43
Marc Kean <marc@marckean.com>
2021-08-24 17:13:52 +1000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -12,7 +12,7 @@ terraform {
     resource_group_name  = "TerraformState_CloudShell"
     storage_account_name = "tfstatecloudshell2021"
     container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "vWAN_No_AzFW.terraform.tfstate"
   }
 }
 
@@ -25,3 +25,9 @@ provider "azurerm" {
   subscription_id = var.connectivity_subscription_id
   features {}
 }
+
+# Run this in the CLI once prior to Terraform commands to grab the storage account key and apply to the $ACCOUNT_KEY environment variable in the Azure CLI
+/*
+ACCOUNT_KEY=$(az storage account keys list --resource-group "TerraformState_CloudShell" --account-name "tfstatecloudshell2021" --query '[0].value' -o tsv)
+export ARM_ACCESS_KEY=$ACCOUNT_KEY
+*/
